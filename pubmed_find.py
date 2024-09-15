32f95fd231cf0d6d80b14de1f16b5beed8d89fa6
Jilin <jilin@bu.edu>
2024-09-15 14:51:25 -0400

    modified script to export to excel spreadsheet

--- pubmed_find.py
+++ pubmed_find.py
@@ -1,6 +1,8 @@
 import re
 import json
 
+from pandas import DataFrame
+
 
 def read_entries(file):
     """
@@ -19,23 +21,34 @@ def process_entries(entries, valid_words, invalid_words):
     """
     create a list of dicts of valid entries' citation, title and author
     """
-    valid_entries = []
+    #valid_entries = []
+    excel_entries = {'Title':[],
+                     'Author':[],
+                     'Citation':[]}
     
     for entry in entries:
         # check for valid and invalid words
         is_valid = any(re.search(r'\b' + re.escape(word) + r'\b', entry, re.IGNORECASE) for word in valid_words)
         is_invalid = any(re.search(r'\b' + re.escape(word) + r'\b', entry, re.IGNORECASE) for word in invalid_words)
-        
+
         if is_valid and not is_invalid:
             # entry contents separated by two newlines
             entry_content = entry.split('\n\n')
+            """
             valid_entries.append({
                 'citation':entry_content[0].split('doi')[0], # preserve up to doi link
                 'title':entry_content[1].replace('\n',''), # delete newline
                 'author':entry_content[2].split('(')[0]
             })
+            """
+            excel_entries['Title'].append(entry_content[1].replace('\n',''))
+            excel_entries['Citation'].append(entry_content[0].split('doi')[0].partition('.')[2].strip())
+            excel_entries['Author'].append(entry_content[2].split('(')[0])
 
-    return valid_entries
+    df = DataFrame(excel_entries)
+    df.to_excel('pubmed_find_results.xlsx', sheet_name='sheet1', index=False)
+
+    #return valid_entries
 
 
 file = 'pubmed_data.txt'
@@ -43,7 +56,10 @@ valid_words = ['dietary intake', 'diet intake', 'food intake', 'energy intake', 
 invalid_words = ["ffq", "food frequency questionnaire"]
 
 entries = read_entries(file)
-result = process_entries(entries, valid_words, invalid_words)
+#result = process_entries(entries, valid_words, invalid_words)
+process_entries(entries, valid_words, invalid_words)
 
-with open('./results.json','w',encoding='utf-8') as f:
+"""
+with open('./pubmed_find_results.json','w',encoding='utf-8') as f:
     f.write(json.dumps(result, indent=4))
+"""
