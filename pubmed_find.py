b1d222896afdb3a4e75c3201b4c907c2207e5c7d
Jilin <jilin@bu.edu>
2024-09-22 14:56:06 -0400

    update script to include abstract

--- pubmed_find.py
+++ pubmed_find.py
@@ -1,10 +1,17 @@
 import re
-from pandas import DataFrame
+import os
+import pandas
+
+
+# SET APPROPRIATELY
+FILES = ['abstract-nutritiona-set 2007 to 2014 12 31.txt', 'abstract-nutritiona-set 2014 to 2019 12 31.txt', 'abstract-nutritiona-set 2019 to 2024 07 21.txt']
+VALID_WORDS = ['dietary intake', 'diet intake', 'food intake', 'energy intake', 'eating intake', 'eat intake', 'dietary pattern', 'diet pattern', 'eating pattern', 'weekend', 'weekday', 'dietary intakes', 'diet intakes', 'food intakes', 'energy intakes', 'eating intakes', 'eat intakes', 'dietary patterns', 'diet patterns', 'eating patterns', 'weekends', 'weekdays', 'workday', 'workdays', 'offdays', 'offday']
+INVALID_WORDS = ['ffq', 'food frequency questionnaire', 'food frequency questionnaires']
 
 
 def read_entries(file):
     """
-    reads the text file containing numbered entries, separating them using three newlines.
+    reads the text file containing numbered entries, separating them using three newlines
     """
     with open(file, 'r', encoding='utf-8', errors='ignore') as f:
         content = f.read()
@@ -17,12 +24,13 @@ def read_entries(file):
 
 def process_entries(entries, valid_words, invalid_words, filename):
     """
-    create a list of dicts of valid entries' citation, title and author
+    perform search through all entries, extracting all valid ones and desired content
     """
-    #valid_entries = []
+    # create a list of dicts of valid entries' citation, title and author
     excel_entries = {'Title':[],
                      'Author':[],
-                     'Citation':[]}
+                     'Citation':[],
+                     'Abstract':[]}
     
     for entry in entries:
         # check for valid and invalid words
@@ -32,34 +40,36 @@ def process_entries(entries, valid_words, invalid_words, filename):
         if is_valid and not is_invalid:
             # entry contents separated by two newlines
             entry_content = entry.split('\n\n')
-            """
-            valid_entries.append({
-                'citation':entry_content[0].split('doi')[0], # preserve up to doi link
-                'title':entry_content[1].replace('\n',''), # delete newline
-                'author':entry_content[2].split('(')[0]
-            })
-            """
             excel_entries['Title'].append(entry_content[1].replace('\n',''))
             excel_entries['Citation'].append(entry_content[0].split('doi')[0].partition('.')[2].strip())
             excel_entries['Author'].append(entry_content[2].split('(')[0])
+            try:
+                abstract = entry_content[4]
+                if "Comment in" in abstract[:10] or "Comment on" in abstract[:10]:
+                    abstract = entry_content[5]
+                excel_entries['Abstract'].append(abstract.replace('\n',''))
+            except:
+                excel_entries['Abstract'].append('ERROR: Failed to find abstract...')
+    
+    return excel_entries
 
-    df = DataFrame(excel_entries)
+
+def save_to_excel(excel_entries):
+    """
+    save a dict of lists into excel
+    """
+    # delete the results file previously generated if it exists
+    if f'results_{filename}.xlsx' in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+        os.remove(f'results_{filename}.xlsx')
+
+    # convert data to pandas DataFrame and save into excel sheet
+    df = pandas.DataFrame(excel_entries)
     df.to_excel(f'results_{filename}.xlsx', sheet_name='sheet1', index=False, engine='xlsxwriter')
 
-    #return valid_entries
 
-
-files = ['abstract-nutritiona-set 2007 to 2014 12 31.txt', 'abstract-nutritiona-set 2014 to 2019 12 31.txt']
-valid_words = ['dietary intake', 'diet intake', 'food intake', 'energy intake', 'eating intake', 'eat intake', 'dietary pattern', 'diet pattern', 'eating pattern', 'weekend', 'weekday', 'dietary intakes', 'diet intakes', 'food intakes', 'energy intakes', 'eating intakes', 'eat intakes', 'dietary patterns', 'diet patterns', 'eating patterns', 'weekends', 'weekdays', 'workday', 'workdays', 'offdays', 'offday']
-invalid_words = ['ffq', 'food frequency questionnaire', 'food frequency questionnaires']
-
-for file in files:
-    entries = read_entries(file)
-    #result = process_entries(entries, valid_words, invalid_words)
-    filename = file.partition('.txt')[0]
-    process_entries(entries, valid_words, invalid_words, filename)
-
-"""
-with open('./pubmed_find_results.json','w',encoding='utf-8') as f:
-    f.write(json.dumps(result, indent=4))
-"""
+if __name__ == '__main__':
+    for file in FILES:
+        entries = read_entries(file) # split text file into individual entries
+        filename = file.partition('.txt')[0] # filename for the generated excel sheet
+        processed_entries = process_entries(entries, VALID_WORDS, INVALID_WORDS, filename)
+        save_to_excel(processed_entries)
